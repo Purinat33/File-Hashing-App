c0564dd4ac92990bcf825d3808ab7b2686e8c1c7
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-01-23 00:19:29 +0700

    Initial Commit

---
+++ fileHashCheck.py
@@ -0,0 +1,92 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+import hashlib
+import zlib
+
+
+# https://www.geeksforgeeks.org/file-explorer-in-python-using-tkinter/
+def openFile():
+
+    file = filedialog.askopenfilename(initialdir='.',
+                                      title="Select File",
+                                      filetypes=(("all files",
+                                                  "*.*"),
+                                                 ("bin files",
+                                                  "*.exe*")
+                                                 ))
+
+    # https://stackoverflow.com/questions/35483700/python-how-to-get-the-filename-in-tkinter-file-dialog
+    file_name = os.path.split(file)[1]
+    label_file.configure(text=f"File: {file_name}")
+
+    # Check stuff
+    if file:
+        # https://stackoverflow.com/questions/3842155/is-there-a-way-to-make-the-tkinter-text-widget-read-only
+        # MD5
+        # https://stackoverflow.com/questions/16874598/how-to-calculate-the-md5-checksum-of-a-file-in-python
+        md5_val = hashlib.md5(open(file, 'rb').read()).hexdigest()
+        label_md5.config(state=tk.NORMAL)
+        label_md5.delete('1.0', tk.END)
+        # https://discuss.python.org/t/tkinter-text-insert/44527
+        label_md5.insert('1.0', md5_val)
+        label_md5.config(state=tk.DISABLED)
+        # CRC
+        crc_val = zlib.crc32(open(file, 'rb').read())
+        label_crc.config(state=tk.NORMAL)
+        label_crc.delete('1.0', tk.END)
+        label_crc.insert('1.0', crc_val)
+        label_crc.config(state=tk.DISABLED)
+
+        # SHA256
+        sha_val = hashlib.sha256(open(file, 'rb').read()).hexdigest()
+        label_sha.config(state=tk.NORMAL)
+        label_sha.delete('1.0', tk.END)
+        label_sha.insert('1.0', sha_val)
+        label_sha.config(state=tk.DISABLED)
+
+        # TODO:
+        # Add to history
+        # Checking against history?
+
+
+# Window
+app = tk.Tk()
+app.title("File Hashing Checker")
+app.geometry("470x600")
+
+
+label = tk.Label(app, text='File Hash Checking', font=("Ariel", 14))
+label.pack(pady=20)
+
+author = tk.Label(app, text="By Purinat33", font=('Ariel', 10))
+author.pack(pady=10)
+
+label_file = tk.Button(app,
+                       text="Open File",
+                       font=("Ariel", 14),
+                       command=openFile)
+
+label_file.pack(pady=10)
+
+
+label_md5_1 = tk.Label(app, text="MD5", font=("Ariel", 14))
+label_md5_1.pack(pady=10)
+
+# https://www.geeksforgeeks.org/python-tkinter-text-widget/
+label_md5 = tk.Text(app, height=1, width=32, font=("Ariel", 14))
+label_md5.pack()
+
+label_crc_1 = tk.Label(app, text="CRC32", font=("Ariel", 14))
+label_crc_1.pack(pady=10)
+
+label_crc = tk.Text(app, height=1, width=10, font=("Ariel", 14))
+label_crc.pack()
+
+label_sha_1 = tk.Label(app, text="SHA256", font=("Ariel", 14))
+label_sha_1.pack(pady=10)
+
+label_sha = tk.Text(app, height=2, width=32, font=("Ariel", 14))
+label_sha.pack()
+
+app.mainloop()
